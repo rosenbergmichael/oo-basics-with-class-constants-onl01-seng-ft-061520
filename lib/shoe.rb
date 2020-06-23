a266ecfcfa36ff81ed001eb819a073a9da5b731a
Michael Rosenberg <rosenbergmj1@gmail.com>
2020-06-23 22:11:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
   
   def initialize(brand)
     @brand = brand
+    BRANDS << brand
   end
 
   def cobble
@@ -13,9 +14,4 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand 
-    BRANDS << brand 
-  end 
-  
 end
